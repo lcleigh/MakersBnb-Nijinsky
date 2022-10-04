85c54b0249dfbe5ef4d2757c2e7a34af669859f1
laura.leigh <lauracleigh314@gmail.com>
2022-10-04 16:13:16 +0100

    make snippet

--- lib/make_snippet.rb
+++ lib/make_snippet.rb
@@ -1,6 +1,11 @@
-# Takes a string as an argument
-# returns the first five words then "..." if there are more than 5
-
-def make_snippet(str)
-    return str
+def make_snippet(string)
+    listofwords = string.split(" ")
+    numberofwords = listofwords.count()
+    if numberofwords > 5
+       firstfivewords = listofwords[0,5].join(" ")
+       return firstfivewords + "..."
+    else
+        return string
+    end
 end
+puts make_snippet("words word wordsss hello four more words")
