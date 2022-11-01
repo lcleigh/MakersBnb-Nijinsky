cb154a9dafa481d724fa09a9fb510fde13ac22f0
annaobarzanowska <anna.obarzanowska@gmail.com>
2022-11-01 15:22:15 +0000

    built a barebones welcome page

--- seeds/seeds_spaces.sql
+++ seeds/seeds_spaces.sql
@@ -2,3 +2,4 @@ TRUNCATE TABLE spaces RESTART IDENTITY;
 
 INSERT INTO spaces (name, price, description, availability, account_id) VALUES ('Treehouse', 200.00, 'Sleep in the trees. See the animals.', 'link to API', '1');
 INSERT INTO spaces (name, price, description, availability, account_id) VALUES ('Lighthouse', 350.00, 'Views of the sea.', 'link to API', '2');
+
--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -10,4 +10,27 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
+  context "Get /" do
+    it "returns a 200 status code" do
+      response = get("/")
+      expect(response.status).to eq(200)
+      expect(response.body).to include "<h1> Feel at home anywhere </h1>"
+    end
+
+    it "shows a list of spaces" do
+      response = get("/")
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include "<a href='/spaces'>View all spaces</a>"
+    end
+  end
+
+  context "/spaces" do
+    it "returns a 200 status code" do
+      response = get("/spaces")
+      
+      expect(response.status).to eq(200)
+      expect(response.body).to include "Treehouse"
+    end
+  end
 end
