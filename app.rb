32efd8392b2345ff38b7a85d67f6287cbef1bd9b
annaobarzanowska <anna.obarzanowska@gmail.com>
2022-11-03 17:20:42 +0000

    added sessions. not working on Anna's machine

--- app.rb
+++ app.rb
@@ -40,7 +40,12 @@ class Application < Sinatra::Base
     end
     
     get '/all_spaces/new_space' do
-      return erb(:new_space_form)
+      if session? == false
+        redirect('/sign_in')
+      else
+        return erb(:new_space_form)
+      end
+      
     end
 
     get '/all_spaces/:id' do
@@ -48,7 +53,7 @@ class Application < Sinatra::Base
       # space_repo = ArtistRepository.new
   
       @space = repo.find(params[:id])
-      # @artist = artist_repo.find(@album.artist_id)
+ 
   
       return erb(:space)
     end
@@ -86,6 +91,8 @@ class Application < Sinatra::Base
       @new_account.phone = params[:phone]
       
       repo.create(@new_account)
+      session_start!
+      session[:user_id] = @new_account.id
       return erb(:post_sign_up)
   
     end
@@ -94,16 +101,27 @@ class Application < Sinatra::Base
       return erb(:sign_in)
     end
 
+    get '/user_homepage' do
+      return erb(:user_homepage)
+    end
+
     post '/sign_in' do
       repo = AccountRepository.new
       @account = repo.find_by_email(params[:email])
 
       if BCrypt::Password.new(@account.password) == params[:password]
-        return erb(:post_sign_in)
+        session_start!
+        session[:user_id] = @account.id
+        return erb(:user_homepage)
       else
         return erb(:sign_in)
       end
-      end
+    end
+    
+    get '/sign_out' do
+      session_end!
+      return erb(:sign_out)
+    end
 
     
 
